96304fe8f7910610e6994cfd4d18127d825a80a0
aymenzoauoui <aymen.zouaoui@esprit.tn>
2024-09-22 18:04:09 +0100

    first commit
    sys solaire

---
+++ Assets/SolarSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    public float earthOrbitSpeed = 10f;   
+    public float earthRotationSpeed = 30f;  // Vitesse de rotation de la Terre sur elle-même
+    public float moonOrbitSpeed = 50f;    // Vitesse de rotation de la Lune autour de la Terre
+    public float moonDistanceFromEarth = 2f;  // Distance de la Lune par rapport à la Terre
+
+    private Vector3 moonInitialPosition;
+
+    void Start()
+    {
+        // Sauvegarder la position initiale de la Lune par rapport à la Terre
+        moonInitialPosition = transform.GetChild(0).GetChild(0).localPosition;
+    }
+
+    // Update est appelé à chaque frame
+    void Update()
+    {
+        // Rotation de la Terre autour du Soleil
+        Transform earth = transform.GetChild(0);
+        earth.RotateAround(transform.position, Vector3.up, earthOrbitSpeed * Time.deltaTime);
+
+        // Rotation de la Terre sur elle-même
+        earth.Rotate(Vector3.up * earthRotationSpeed * Time.deltaTime);
+
+        // Rotation de la Lune autour de la Terre
+        Transform moon = earth.GetChild(0); // Récupère la Lune
+
+        // Rotation autour de la Terre avec une vitesse définie
+        moon.Rotate(Vector3.up * moonOrbitSpeed * Time.deltaTime);
+
+        // Ajuster la position de la Lune pour qu'elle reste à une distance fixe de la Terre
+       // moon.localPosition = moonInitialPosition.normalized * moonDistanceFromEarth;
+    }
+}
